4db1c82048c56d56010859fa3900c772501b959f
duduodudu <duweiquan@foxmail.com>
2021-03-05 22:51:35 +0800

    增加写入日志按钮

--- Logger/app/src/main/java/cn/com/intasect/logapp/MainActivity.java
+++ Logger/app/src/main/java/cn/com/intasect/logapp/MainActivity.java
@@ -3,6 +3,7 @@ package cn.com.intasect.logapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -13,11 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 初始化
+        IscTxtLoggerHelper.getInstance().init(getApplicationContext(), this);
+        writeLog(false);
+
+        findViewById(R.id.button_log).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                writeLog(true);
+            }
+        });
+    }
+
+    private void writeLog(boolean isClickByButton) {
         SimpleDateFormat simpleFormatter = new SimpleDateFormat("HH:mm:ss");
         String now = simpleFormatter.format(new Date(System.currentTimeMillis()));
-        IscTxtLoggerHelper.getInstance().init(getApplicationContext(),this);
-        IscTxtLoggerHelper.getInstance().logInfo("测试信息" + now);
-        IscTxtLoggerHelper.getInstance().logNetwork("测试信息" + now);
+        if (isClickByButton) {
+            IscTxtLoggerHelper.getInstance().logInfo("点击按钮写入日志" + now);
+            IscTxtLoggerHelper.getInstance().logNetwork("点击按钮写入日志" + now);
+        } else {
+            IscTxtLoggerHelper.getInstance().logInfo("程序onCreate写入日志" + now);
+            IscTxtLoggerHelper.getInstance().logNetwork("程序onCreate写入日志" + now);
+        }
+
     }
 
 
